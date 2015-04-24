1365356348129a2ff452e0ee39add07b05c31837
thatoddmailbox <asreg123@gmail.com>
2015-04-24 10:38:25 -0400

    Add database file

---
+++ db.inc.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_BASE", "pHelpdesk");
+define("DB_PREF", "pHelpdesk_");
+
+$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_BASE . ';charset=utf8', DB_USER, DB_PASS, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
