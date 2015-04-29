3441b3440b50222cf8a616d10825010f3aae477f
thatoddmailbox <asreg123@gmail.com>
2015-04-28 22:06:14 -0400

    Begin user management page

--- admin/users.php
+++ admin/users.php
@@ -2,13 +2,60 @@
 <?php require("../requireLogin.inc.php"); ?>
 <div class="container">
 	<h1>User management</h1>
-	<p>Select an option:</p>
-	<ul>
-		<li><a href="<?php echo SITE_URL; ?>admin/users.php">User management</a></li>
-		<li><a href="<?php echo SITE_URL; ?>admin/ticketAssignment.php">Ticket assignment</a></li>
-		<li><a href="<?php echo SITE_URL; ?>admin/knolConfig.php">Knowldegebase configuration</a></li>
-		<li><a href="<?php echo SITE_URL; ?>admin/siteAppearance.php">Site appearance</a></li>
-		<li><a href="<?php echo SITE_URL; ?>admin/siteConfig.php">Site configuration</a></li>
-	</ul>
+	<table class="sortable">
+		<thead>
+			<tr>
+				<th>ID Number</th>
+				<th>Name</th>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Level</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+				$usersStmt = $db->prepare("SELECT * FROM `" . DB_PREF . "accounts`");
+				$usersStmt->execute();
+				$results = $usersStmt->fetchAll(PDO::FETCH_ASSOC);
+
+				foreach ($results as $entry) {
+					echo "<tr>";
+
+					echo "<td>";
+					echo htmlentities($entry["accountId"]);
+					echo "</td>";
+
+					echo "<td>";
+					echo htmlentities($entry["accountName"]);
+					echo "</td>";
+
+					echo "<td>";
+					echo htmlentities($entry["accountUsername"]);
+					echo "</td>";
+
+					echo "<td>";
+					echo htmlentities($entry["accountEmail"]);
+					echo "</td>";
+
+					echo "<td>";
+					echo htmlentities($entry["accountLevel"]);
+					echo "</td>";
+
+					echo "<td>";
+					echo "asdf";
+					echo "</td>";
+
+					echo "</tr>";
+				}
+			?>
+		</tbody>
+	</table>
 </div>
+<script src="<?php echo SITE_URL; ?>js/sorttable.js"></script>
+<style>
+th {
+	min-width: 170px;
+}
+</style>
 <?php require("../footer.inc.php"); ?>
