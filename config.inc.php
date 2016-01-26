d54080176e36f8e00bd3fcda068be64c4f4c44ef
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-26 11:47:55 -0500

    sane defaults

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 define("SITE_NAME", "pHelpdesk Test");
-define("SITE_URL", "http://localhost:8888/pHelpdesk/"); // Don't forget the trailing slash!
+define("SITE_URL", "http://localhost/pHelpdesk/"); // Don't forget the trailing slash!
 define("SITE_COPYRIGHT", "Copyright © 2015 Support Company");
 
 define("SITE_HASH_COST", 8);
